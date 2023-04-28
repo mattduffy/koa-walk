d698bcd776a9e79f2ad03588b182a473f20d5069
Matthew Duffy <mattduffy@gmail.com>
2023-04-27 21:23:44 -0700

    Refactor the event-handler functions for the modal dialog box.

--- public/j/adminlist.js
+++ public/j/adminlist.js
@@ -1,62 +1,84 @@
 function updateGrid(dataset, target) {
   const div = document.createElement('div')
   div.classList.add('gridspan')
-  // div.setAttribute('class', 'gridspan')
-  // div.className = 'gridspan'
   const text = document.createTextNode(`Permanently deleted ${dataset.username}.  No going back...`)
   div.appendChild(text)
   target.parentNode.insertBefore(div, target)
   console.log(div)
 }
 
-function doDelete(dataset, node) {
+function doDelete(dataset, row) {
   if (!dataset) {
     console.error('No dataset values provided.  No delete issued.')
     return false
   }
   console.log('doDelete called with: ')
   console.table(dataset)
-  updateGrid(dataset, node)
+  updateGrid(dataset, row.usernameNode)
   return true
 }
 
+function createDialog(dataset) {
+  console.log(this)
+  const spanPrompt = document.createElement('span')
+  spanPrompt.setAttribute('id', 'prompt')
+  const dialogText = `You are about to delete <em>${dataset.username}'s</em> account.<br>`
+    + 'This action cannot be undone.<br>'
+    + 'Are you sure you want to delete this account?<br>'
+  spanPrompt.innerHTML = dialogText
+  const spanYes = document.createElement('span')
+  spanYes.innerHTML = 'Yes'
+  spanYes.setAttribute('id', 'yes')
+  spanYes.classList.add('dialogYes')
+  const spanNo = document.createElement('span')
+  spanNo.innerHTML = 'No'
+  spanNo.setAttribute('id', 'no')
+  spanNo.classList.add('dialogNo')
+  const dialog = document.createElement('dialog')
+  dialog.setAttribute('data-uid', dataset.id)
+  dialog.appendChild(spanPrompt)
+  dialog.appendChild(document.createElement('br'))
+  dialog.appendChild(spanNo)
+  dialog.appendChild(spanYes)
+  document.body.appendChild(dialog)
+  spanYes.addEventListener('click', (y) => {
+    y.preventDefault()
+    y.stopPropagation()
+    dialog.close('yes')
+  })
+  spanNo.addEventListener('click', (n) => {
+    n.preventDefault()
+    n.stopPropagation()
+    dialog.close('no')
+  })
+  return dialog
+}
+
+const trashcans = document.querySelectorAll('img.trashcan')
 window.addEventListener('load', () => {
-  const trashcans = document.querySelectorAll('img.trashcan')
-  // for (const can of trashcans.entries()) {
   trashcans.forEach((can) => {
     console.log(can.dataset)
     can.addEventListener('click', (e) => {
       e.stopPropagation()
       e.preventDefault()
-      const editNode = can.parentNode.previousElementSibling
-      const viewNode = editNode.previousElementSibling
-      const statusNode = viewNode.previousElementSibling
-      const usernameNode = statusNode.previousElementSibling
-      const deletePrompt = `You are about to delete <em>${usernameNode.firstChild.text}'s</em> account.<br>`
-        + 'This action cannot be undone.<br>'
-        + 'Are you sure you want to delete this account?<br>'
-      const dialog = document.querySelector('dialog[id="dia"]')
-      dialog.firstElementChild.innerHTML = deletePrompt
-      const yes = document.querySelector('span[id="yes"]')
-      yes.addEventListener('click', (e) => {
-        e.preventDefault()
-        e.stopPropagation()
-        dialog.close('yes')
-      })
-      const no = document.querySelector('span[id="no"]')
-      no.addEventListener('click', (e) => {
-        e.preventDefault()
-        e.stopPropagation()
-        dialog.close('no')
-      })
-      dialog.addEventListener('close', (e) => {
-        console.info(`dialog.returnValue: ${dialog.returnValue}`)
-        if (dialog.returnValue === 'yes') {
-          console.log(can.dataset)
-          doDelete(can.dataset, usernameNode)
+      const thisRow = {
+        canNode: can.parentNode,
+        editNode: can.parentNode.previousElementSibling,
+        viewNode: can.parentNode.previousElementSibling.previousElementSibling,
+        statusNode: can.parentNode.previousElementSibling.previousElementSibling.previousElementSibling,
+        usernameNode: can.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling,
+      }
+      console.log(thisRow)
+      const modalDialog = createDialog(can.dataset)
+      modalDialog.addEventListener('close', (c) => {
+        console.info(`dialog.returnValue: ${modalDialog.returnValue}`)
+        if (modalDialog.returnValue === 'yes') {
+          console.log('Dialog Yes clicked')
+          doDelete(can.dataset, thisRow)
+          // removeEventHandler
         }
       })
-      dialog.showModal()
+      modalDialog.showModal()
     })
   })
 })
