6b7fcad18ad527ca24d6a112e0489e160630e9ec
Matthew Duffy <mattduffy@gmail.com>
2023-06-07 21:57:17 -0700

    Add generateKey class method which proxies the reqeust to private methods for each crypto key type.

--- src/models/cryptoKeys.js
+++ src/models/cryptoKeys.js
@@ -16,34 +16,62 @@ const DATABASE = process.env.MONGODB_DBNAME ?? 'koastub'
 const COLLECTION = 'app'
 
 class CryptoKeys {
-  constructor(config = {}) {
+  #db
+
+  #siteName
+
+  #keys
+
+  #rsaEnc
+
+  #rsaSig
+
+  #bits
+
+  #hash
+
+  #curve
+
+  _pubKey
+
+  _priKey
+
+  _jwk
+
+  constructor(config) {
     const log = keysLog.extend('constructor')
     const error = keysError.extend('constructor')
 
-    this._keyDir = config.keyDir
-    this._db = config?.db.db().collection(COLLECTION)
-    this._siteName = process.env.SITE_NAME ?? 'website'
-    this._keys = config.keys ?? { signing: [], encrypting: [] }
+    this._format = config?.format ?? 'pem'
+    this._keyDir = config?.keyDir ?? process.env.KEY_DIR ?? './keys'
+    this._kid = ulid()
+    this.#db = config?.db?.db().collection(COLLECTION) ?? null
+    this.#siteName = process.env.SITE_NAME ?? 'website'
+    this.#keys = config.keys ?? { signing: [], encrypting: [] }
+    this.#rsaEnc = process.env.RSA_ENC_KEY_NAME ?? 'RSA-OAEP'
+    this.#rsaSig = process.env.RSA_SIG_KEY_NAME ?? 'RSASSA-PKCS1-v1_5'
+    this.#bits = process.env.RSA_SIG_KEY_MOD ?? 2048
+    this.#hash = process.env.RSA_ENC_KEY_TYPE ?? 'SHA-256'
+    this.#curve = process.env.ECDSA_SIG_KEY_NAMEDCURVE ?? 'P-521'
   }
 
-  async generateKey(config = { use: 'sig', alg: 'RSA' }) {
+  async generateKey(o) {
     const log = keysLog.extend('generateKey')
     const error = keysError.extend('generateKey')
+    const options = { use: 'sig', alg: 'RSA', ...o }
     let keys
-    if (config.alg.toLowerCase() === 'rsa') {
-      if (config.use === 'sig') {
+    if (options.alg.toLowerCase() === 'rsa') {
+      if (options.use === 'sig') {
         keys = await this.#signRSA()
-      } else if (config.use === 'enc') {
+      } else if (options.use === 'enc') {
         keys = await this.#encRSA()
       } else {
-        const errorMessage = `Invalid key use: ${config.use} specified in parameters.`
+        const errorMessage = `Invalid key use: ${options.use} specified in parameters.`
         error(errorMessage)
         throw new Error(errorMessage)
       }
-    } else if (config.alg.toLowerCase() === 'ecdsa') {
-      const errorMessage = 'ECDSA keys no implemented yet.'
-      error(errorMessage)
-      throw new Error(errorMessage)
+    } else if (options.alg.toLowerCase() === 'ecdsa') {
+      keys = await this.#sigECDSA()
     } else {
       const errorMessage = 'Missing required key algorith.'
       error(errorMessage)
@@ -51,13 +79,72 @@ class CryptoKeys {
     }
     return keys
   }
-  
+
   async #signRSA() {
-    
+    let keys
+    try {
+      keys = await subtle.generateKey(
+        {
+          name: this.#rsaSig,
+          modulusLength: this.#bits,
+          publicExponent: new Uint8Array([1, 0, 1]),
+          hash: this.#hash,
+        },
+        true,
+        ['sign', 'verify'],
+      )
+      this._keyType = this.#rsaSig
+      this._pubKey = keys.publicKey
+      this._priKey = keys.privateKey
+    } catch (e) {
+      throw new Error(e)
+    }
+    // return keys
+    return this
   }
 
   async #encRSA() {
+    let keys
+    try {
+      keys = await subtle.generateKey(
+        {
+          name: this.#rsaEnc,
+          modulusLength: this.#bits,
+          publicExponent: new Uint8Array([1, 0, 1]),
+          hash: this.#hash,
+        },
+        true,
+        ['encrypt', 'decrypt'],
+      )
+      this._keyType = this.#rsaEnc
+      this._pubKey = keys.publicKey
+      this._priKey = keys.privateKey
+    } catch (e) {
+      throw new Error(e)
+    }
+    // return keys
+    return this
+  }
 
+  async #sigECDSA() {
+    let keys
+    try {
+      keys = await subtle.generateKey(
+        {
+          name: 'ECDSA',
+          namedCurve: this.#curve,
+        },
+        true,
+        ['sign', 'verify'],
+      )
+      this._keyType = 'ECDSA'
+      this._pubKey = keys.publicKey
+      this._priKey = keys.privateKey
+    } catch (e) {
+      throw new Error(e)
+    }
+    // return keys
+    return this
   }
 }
 
