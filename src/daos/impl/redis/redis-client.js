367410a55c9b70159cec74db0668f4176fff54c6
Matthew Duffy <mattduffy@gmail.com>
2023-08-28 19:58:03 -0700

    Refactor the redis client connection to use Dotenv env variable encapusulation.

--- src/daos/impl/redis/redis-client.js
+++ src/daos/impl/redis/redis-client.js
@@ -16,32 +16,33 @@ const __dirname = path.dirname(__filename)
 const root = path.resolve(`${__dirname}/../../../..`)
 console.log('redis-client.js >>root = ', root)
 const showDebug = process.env.NODE_ENV !== 'production'
-Dotenv.config({ path: path.resolve(root, 'config/redis.env'), debug: showDebug })
+const redisEnv = {}
+Dotenv.config({ path: path.resolve(root, 'config/redis.env'), processEnv: redisEnv, debug: showDebug })
 
-const sentinelPort = process.env.SENTINEL_PORT ?? 36379
+const sentinelPort = redisEnv.SENTINEL_PORT ?? 36379
 const redisConnOpts = {
   sentinels: [
-    { host: process.env.REDIS_SENTINEL_01, port: sentinelPort },
-    { host: process.env.REDIS_SENTINEL_02, port: sentinelPort },
-    { host: process.env.REDIS_SENTINEL_03, port: sentinelPort },
+    { host: redisEnv.REDIS_SENTINEL_01, port: sentinelPort },
+    { host: redisEnv.REDIS_SENTINEL_02, port: sentinelPort },
+    { host: redisEnv.REDIS_SENTINEL_03, port: sentinelPort },
   ],
   name: 'myprimary',
-  db: process.env.REDIS_DB,
-  keyPrefix: `${process.env.REDIS_KEY_PREFIX}:` ?? 'koa:',
-  sentinelUsername: process.env.REDIS_SENTINEL_USER,
-  sentinelPassword: process.env.REDIS_SENTINEL_PASSWORD,
-  username: process.env.REDIS_USER,
-  password: process.env.REDIS_PASSWORD,
+  db: redisEnv.REDIS_DB,
+  keyPrefix: `${redisEnv.REDIS_KEY_PREFIX}:` ?? 'koa:',
+  sentinelUsername: redisEnv.REDIS_SENTINEL_USER,
+  sentinelPassword: redisEnv.REDIS_SENTINEL_PASSWORD,
+  username: redisEnv.REDIS_USER,
+  password: redisEnv.REDIS_PASSWORD,
   connectionName: 'redis-om',
   enableTLSForSentinelMode: true,
   sentinelRetryStrategy: 100,
   tls: {
-    ca: await fs.readFile(process.env.REDIS_CACERT),
+    ca: await fs.readFile(redisEnv.REDIS_CACERT),
     rejectUnauthorized: false,
     requestCert: true,
   },
   sentinelTLS: {
-    ca: await fs.readFile(process.env.REDIS_CACERT),
+    ca: await fs.readFile(redisEnv.REDIS_CACERT),
     rejectUnauthorized: false,
     requestCert: true,
   },
