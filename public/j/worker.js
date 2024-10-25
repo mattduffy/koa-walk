ce92ab25d9faaa9ca16037b2eccba18e54869fc9
Matthew Duffy <mattduffy@gmail.com>
2024-10-24 21:53:49 -0700

    Bump

--- public/j/worker.js
+++ public/j/worker.js
@@ -3,6 +3,6 @@
  */
 onmessage = function(e) {
   console.log('Worker: messagee received from main script.')
-  console.log(e.data)
+  console.log(`message data: ${e.data}`)
   postMessage('Take a walk.')
 }
