2b443cf4c25071e50d2bfcbde0786f27d7922f6e
Matthew Duffy <mattduffy@gmail.com>
2024-10-25 22:36:23 -0700

    Testing out getCurrentLocation() ability.

--- public/j/worker.js
+++ public/j/worker.js
@@ -2,7 +2,25 @@
  * file: public/j/worker.js
  */
 onmessage = function(e) {
-  console.log('Worker: messagee received from main script.')
-  console.log(`message data: ${e.data}`)
+  switch (e.data.TASK) {
+    case 'START_WALK':
+      console.log(e.data.TASK, e.data.msg)
+      break
+    case 'STOP_WALK':
+      console.log(e.data.TASK, e.data.msg)
+      break
+    case 'SET_WAYPOINT':
+      console.log(e.data.TASK, e.data.msg)
+      break
+    case 'SAVE_WALK':
+      console.log(e.data.TASK, e.data.msg)
+      break
+    case 'GET_WALK':
+      console.log(e.data.TASK, e.data.msg)
+      break
+    default:
+      console.log('Worker: messagee received from main script.')
+      console.log(`message data: ${e.data}`)
+  }
   postMessage('Take a walk.')
 }
