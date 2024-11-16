ec159727f0008fe38ba9c38d4f330ed063ef09d7
Matthew Duffy <mattduffy@gmail.com>
2024-11-15 20:50:55 -0800

    bump

--- public/j/worker.js
+++ public/j/worker.js
@@ -3,30 +3,42 @@
  */
 /* eslint-env worker */
 onmessage = (e) => {
-  console.log(e.data.TASK)
-  switch (e.data.TASK) {
-    case 'START_WALK':
-      console.log(e.data.TASK, e.data.msg)
-      break
-    case 'STOP_WALK':
-      console.log(e.data.TASK, e.data.msg)
-      break
-    case 'SET_WAYPOINT':
-      console.log(e.data.TASK, e.data.msg)
-      break
-    case 'SAVE_WALK':
-      console.log(e.data.TASK, e.data.msg)
-      break
-    case 'GET_WALK':
-      console.log(e.data.TASK, e.data.msg)
-      break
-    default:
-      console.info(e)
-      console.warn('Worker: received an unknown task from the main script.')
-      console.warn('message data: ', e.data)
-      throw new Error(`unsupported task: ${e.data.TASK}`)
+  console.log(self.name)
+  console.log(e.data)
+  if (e.data?.TASK) {
+    switch (e.data.TASK) {
+      case 'LOGIN':
+        try {
+          const request = new Request()
+        } catch (err) {
+          console.log(err)
+          postMessage({ err: 'login failed' })
+        }
+        break
+      case 'START_WALK':
+        console.log(e.data.TASK, e.data.msg)
+        break
+      case 'STOP_WALK':
+        console.log(e.data.TASK, e.data.msg)
+        break
+      case 'SET_WAYPOINT':
+        console.log(e.data.TASK, e.data.msg)
+        break
+      case 'SAVE_WALK':
+        console.log(e.data.TASK, e.data.msg)
+        break
+      case 'GET_WALK':
+        console.log(e.data.TASK, e.data.msg)
+        break
+      default:
+        console.info(e)
+        console.warn('Worker: received an unknown task from the main script.')
+        console.warn('message data: ', e.data)
+        throw new Error(`unsupported task: ${e.data.TASK}`)
+    }
   }
 }
+
 // onerror = (error) => {
 //   console.warn('oops')
 //   console.warn(error)
