2e98cc0b05a799fe020b048d3535200d154a485d
Matthew Duffy <mattduffy@gmail.com>
2025-01-22 21:51:31 -0800

    Add set/getCurrentPosition() methods to the State class.

--- public/j/State.js
+++ public/j/State.js
@@ -12,6 +12,27 @@ class State extends Subject {
     this.notify(this.state)
   }
 
+  getCurrentPosition() {
+    return this.state.currentPosition
+  }
+
+  setCurrentPosition(c = {}) {
+    console.log('setting current position to: ', c)
+    console.log('c.contructor.name === ', c.constructor.name)
+    if (c.constructor.name === 'GeolocationPosition') {
+      console.log('normalizing GeoLocationPosition into smaller obj.')
+      this.state.currentPosition = {
+        latitude: c.coords.latitude,
+        longitude: c.coords.longitude,
+        accuracy: c.coords.accuracy,
+        timestamp: c.timestamp,
+      }
+    } else {
+      this.state.currentPosition = c
+    }
+    console.log('current position is now: ', this.state.currentPosition)
+  }
+
   addPoint(point) {
     if (!this.state?.wayPoints) {
       this.state.wayPoints = []
