c98100911b320db24b6e7c2f85dd128b12c31142
Matthew Duffy <mattduffy@gmail.com>
2025-02-01 20:22:51 -0800

    Add pointDistance() function to calculate the distance between two point coordinates.

--- public/j/Heading.js
+++ public/j/Heading.js
@@ -1,6 +1,30 @@
-export function pointDistance(p1, p2) {
-  console.log('p1: ', p1)
-  console.log('p2: ', p2)
+function rads(degrees) {
+  return degrees * (Math.PI / 180)
+}
+
+export function pointDistance(p1, p2, u = 'metric') {
+  console.log('pointDistances(p1, p2, u): ', p1, p2, u)
+  const earthRadiusKm = 6371
+  const earthRadiusMeters = 6371000
+  const earthRadiusMi = 3959
+  const _u = u.toLowerCase()
+  let r
+  if (_u === 'metric' || _u === 'meters') {
+    r = earthRadiusMeters
+  } else if (_u === 'km') {
+    r = earthRadiusKm
+  } else if (_u === 'miles' || _u === 'mi') {
+    r = earthRadiusMi
+  }
+  console.log('using earth radius: ', r)
+  const dLat = rads(p2.latitude - p1.latitude)
+  const dLon = rads(p2.longitude - p1.longitude)
+  const lat1 = rads(p1.latitude)
+  const lat2 = rads(p2.latitude)
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+          + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  return r * c
 }
 
 export function heading(p1, p2) {
