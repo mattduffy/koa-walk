a39c630e7e0e98574f4a58ece27497d08aada72e
Matthew Duffy <mattduffy@gmail.com>
2025-02-26 21:00:19 -0800

    First pass at generating a kml animated track from walk data.

--- public/j/State.js
+++ public/j/State.js
@@ -222,12 +222,84 @@ class State extends Subject {
     }
   }
 
+  get kmlTrack() {
+    const fmt = {year: 'numeric', month: 'short', day: 'numeric'}
+    const niceDate = new Date(this.state.date)
+      .toLocaleString('en-US', fmt)
+    const last = this.state.wayPoints.length - 1
+    let kml = 
+`<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2"
+  xmlns:gx="http://www.google.com/kml/ext/2.2"
+  xmlns:atom="http://www.w3.org/2005/Atom">
+  <Document>
+    <atom:author>
+      <atom:name>Matthew Duffy</atom:name>
+    </atom:author>
+    <atom:link href="http://walk.genevalakepiers.com" />
+    <open>1</open>
+    <name>${this.state.name} walk</name>
+    <visibility>1</visibility>
+    <description>
+      <![CDATA[<h3>${this.state.name}</h3>
+      <h4>${this.state.location}</h4>
+      <h4>${niceDate}</h4>
+      <p>Duration ${new Date(this.state.duration)
+          .toISOString().slice(11, 19)}</p> 
+      <p>Distance ${this.state.distance.toFixed(1)} meters</p>]]>
+    </description>
+    <LookAt>
+      <gx:TimeSpan>
+        <begin>${new Date(this.state.startTime).toISOString()}</begin>
+        <end>${new Date(this.state.endTime).toISOString()}</end>
+      </gx:TimeSpan>
+      <longitude>${this.state.wayPoints[0].longitude}</longitude>
+      <latitude>${this.state.wayPoints[0].latitude}</latitude>
+      <range>1300.000000</range> 
+    </LookAt> 
+    <Style id="check-hide-children">
+      <ListStyle>
+        <listItemType>checkHideChildren</listItemType>
+      </ListStyle>
+    </Style>
+    <styleUrl>#check-hide-children</styleUrl>
+    <Style id="lineStyle">
+      <LineStyle>
+        <color>ffD94F32</color>
+        <width>6</width>
+      </LineStyle>
+    </Style>
+    <Folder>
+      <name>Track</name>
+      <Placemark> 
+        <name>${new Date(this.state.date).toISOString()}</name>
+        <gx:Track id="theWalk">
+          <altitudeMode>clampToGround</altitudeMode>
+          ${this.state.wayPoints.map((w) => {
+            return '<when>' + new Date(w.timestamp).toISOString() + '</when>'
+          }).join('\n')}
+          ${this.state.wayPoints.map((w) => {
+            return '<gx:coord>'
+              + w.longitude
+              + w.latitude
+              + '0'
+              + '</gx:coord>'
+          }).join('\n')}
+        </gx:Track> 
+      </Placemark>
+    </Folder>
+  </Document>
+</kml>
+`
+    return kml
+  }
+
   get kmlLineString() {
     const fmt = {year: 'numeric', month: 'short', day: 'numeric'}
-    const niceDate = new Date(walk.features[0].properties.date)
+    const niceDate = new Date(this.state.date)
       .toLocaleString('en-US', fmt)
-    const last = walk.features[0].geometry.coordinates.length - 1
-    kml = 
+    const last = this.state.wayPoints.length - 1
+    let kml = 
 `<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2"
   xmlns:atom="http://www.w3.org/2005/Atom">
@@ -240,7 +312,6 @@ class State extends Subject {
     <Style id="myWalkStyle">
       <LineStyle id="walk">
         <color>ffD94F32</color>
-        <colorMode>normal</colorMode>
         <width>5.0</width>
       </LineStyle>
     </Style>
@@ -261,20 +332,20 @@ class State extends Subject {
       </IconStyle>
     </Style>
     <Placemark>
-      <name>${walk.features[0].properties.name} walk</name>
+      <name>${this.state.name} walk</name>
       <styleUrl>#myWalkStyle</styleUrl> 
       <visibility>1</visibility>
       <description><![CDATA[
-        <h3>${walk.features[0].properties.name}</h3>
-        <h4>${walk.features[0].properties.location}</h4>
+        <h3>${this.state.name}</h3>
+        <h4>${this.state.location}</h4>
         <h4>${niceDate}</h4>
-        <p>Duration ${new Date(walk.features[0].properties.duration)
+        <p>Duration ${new Date(this.state.duration)
             .toISOString().slice(11, 19)}</p> 
-        <p>Distance ${walk.features[0].properties.distance.toFixed(1)} meters</p>
+        <p>Distance ${this.state.distance.toFixed(1)} meters</p>
       ]]></description>
       <LookAt>
-        <longitude>${walk.features[0].geometry.coordinates[0][0]}</longitude>
-        <latitude>${walk.features[0].geometry.coordinates[0][1]}</latitude>
+        <longitude>${this.state.wayPoints[0].longitude}</longitude>
+        <latitude>${this.state.wayPoints[0].latitude}</latitude>
         <altitude>100</altitude>
         <heading>0</heading>
         <tilt>0</tilt>
@@ -283,8 +354,8 @@ class State extends Subject {
       <LineString>
         <tessellate>1</tessellate>
         <coordinates>
-          ${walk.features[0].geometry.coordinates.map((c) => {
-            return c[0] + ',' + c[1] + ',0'
+          ${this.state.wayPoints.map((w) => {
+            return w.longitude + ',' + w.latitude + ',0'
           }).join('\n')}
         </coordinates>
       </LineString>
@@ -292,9 +363,13 @@ class State extends Subject {
     <Placemark>
       <name>Start</name>
       <description><![CDATA[
-        <p>Start time: ${new Date(walk.features[0].properties.startTime)
+        <p>Start time: ${new Date(this.state.startTime)
         .toISOString().slice(11, 19)}</p>
-        <p>Start location: ${walk.features[0].geometry.coordinates[0][0]},${walk.features[0].geometry.coordinates[0][1]}</p>
+        <p>
+          Start location: <br>
+          longitude ${this.state.wayPoints[0].longitude}<br>
+          latitude  ${this.state.wayPoints[0].latitude}
+        </p>
         ]]>
       </description>
       <Style id="grn-pushpin">
@@ -307,16 +382,20 @@ class State extends Subject {
       </Style>
       <Point>
         <coordinates>
-          ${walk.features[0].geometry.coordinates[0][0]},${walk.features[0].geometry.coordinates[0][1]},0
+          ${this.state.wayPoints[0].longitude},${this.state.wayPoints[0].latitude},0
         </coordinates>
       </Point>
     </Placemark>
     <Placemark>
       <name>Finish</name>
       <description><![CDATA[
-        <p>Finish time: ${new Date(walk.features[0].properties.endTime)
+        <p>Finish time: ${new Date(this.state.endTime)
         .toISOString().slice(11, 19)}</p>
-        <p>Finish location: ${walk.features[0].geometry.coordinates[last][0]},${walk.features[0].geometry.coordinates[last][1]}</p>
+        <p>
+          Finish location:<br>
+          longitude ${this.state.wayPoints[last].longitude}<br>
+          latitude  ${this.state.wayPoints[last].latitude}
+        </p>
         ]]></description>
       <Style id="ylw-pushpin">
         <IconStyle id="myylwpushpin">
@@ -328,13 +407,14 @@ class State extends Subject {
       </Style>
       <Point>
         <coordinates>
-          ${walk.features[0].geometry.coordinates[last][0]},${walk.features[0].geometry.coordinates[last][1]},0
+          ${this.state.wayPoints[last].longitude},${this.state.wayPoints[last].latitude},0
         </coordinates>
       </Point>
     </Placemark>
   </Document>
 </kml>
 `
+    return kml
   }
 
   get() {
