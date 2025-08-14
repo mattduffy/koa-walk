733494bf91e9dcda40f4215c6b5d4a14bbaedef0
Matthew Duffy <mattduffy@gmail.com>
2025-08-13 18:53:15 -0700

    Add ai model set up with new config/ai.env and add details to the ctx.state.

--- src/index.js
+++ src/index.js
@@ -53,6 +53,11 @@ dotenv.config({
   processEnv: appEnv,
   debug: showDebug,
 })
+let aiEnv = {}
+dotenv.config({
+  path: path.resolve(appRoot, 'config/ai.env'),
+  processEnv: aiEnv,
+})
 
 const banner = new Banner({
   name: appEnv.SITE_NAME,
@@ -87,6 +92,7 @@ app.dirs = {
   },
   cache: {
     pages: `${appRoot}/cached_pages`,
+    models: `${appRoot}/${aiEnv.MODEL_CACHE_DIR}`,
   },
   config: `${appRoot}/config`,
   keys: `${appRoot}/keys`,
@@ -104,6 +110,19 @@ app.dirs = {
   },
 }
 appEnv.UPLOADSDIR = app.dirs.private.uploads
+
+async function aiSetup(ctx, next) {
+  ctx.state.ai = {
+    model: aiEnv.EMBEDDING_MODEL,
+    embeddingTask: aiEnv.EMBEDDING_TASK,
+    vectorType: aiEnv.VECTOR_TYPE,
+    vectorAlg: aiEnv.VECTOR_ALGORITHM,
+    vectorDistanceMetric: aiEnv.VECTOR_DIST_METRIC,
+    vectorDIM: aiEnv.VECTOR_DIM,
+  }
+  return next()
+}
+app.use(aiSetup)
 
 const o = {
   db: path.resolve(`${app.root}/src`, 'daos/impl/mongodb/mongo-client.js'),
