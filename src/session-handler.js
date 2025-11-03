128e391e6670ea1c9df4ae894d9c2f8e65652e45
Matthew Duffy <mattduffy@gmail.com>
2025-11-02 20:31:53 -0800

    Add a space after multiply sign (*).

--- src/session-handler.js
+++ src/session-handler.js
@@ -73,7 +73,7 @@ console.log(
 const config = {
   store: redis,
   key: redisEnv.SESSION_KEY ?? 'session',
-  maxAge: redisEnv.SESSION_1_DAY * 1000 * 3 ?? (86400 *1000 * 3),
+  maxAge: redisEnv.SESSION_1_DAY * 1000 * 3 ?? (86400 * 1000 * 3),
   rolling: (redisEnv.SESSION_ROLLING.toLowerCase() === 'true') ?? true,
   renew: (redisEnv.SESSION_RENEW.toLowerCase() === 'true') ?? true,
   overwrite: true,
