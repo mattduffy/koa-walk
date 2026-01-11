afd383e032f3593d6358a10a5ebab66ed1fabf56
Matthew Duffy <mattduffy@gmail.com>
2026-01-10 19:16:25 -0800

    Add eslint-import-resolver-exports package for improved linting, and add npm run lint script.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,14 @@
 const restrictedGlobals = require('eslint-restricted-globals')
+
 module.exports = {
+  settings: {
+    'import/resolver': {
+      exports: {},
+      node: {
+        extensions: ['.js', '.mjs', '.tx', 'tsx'],
+      },
+    },
+  },
   globals: {
     window: true,
     document: true,
@@ -31,6 +40,7 @@ module.exports = {
     'no-console': 'off',
     'no-underscore-dangle': 'off',
     'import/extensions': 'off',
+    'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
     'max-len': ['error', { code: 100 }],
     'new-cap': 'off',
