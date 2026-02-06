e0627968de927cd60ba5360f0649f98d0f1de2b4
Matthew Duffy <mattduffy@gmail.com>
2026-02-05 21:06:07 -0800

    Add new script in src/utils to refactor geo location data - specifically the coordinates array so it carries timestamps and other non-standard geojson values for each point.

---
+++ src/utils/refactorGeoData.js
@@ -0,0 +1,140 @@
+/**
+ * @module @mattduffy/koa-glp
+ * @author Matthew Duffy <mattduffy@gmail.com>
+ * @summary The script to refactor geo data and timestamps to prep for calorie counter.
+ * @file src/utils/refactorGeoData.js
+ */
+
+import path from 'node:path'
+import * as dotenv from 'dotenv'
+import { fileURLToPath } from 'node:url'
+import crypto from 'node:crypto'
+import { Command } from 'commander'
+import * as Users from '@mattduffy/users/Users.js'
+import * as mongoClient from '../daos/impl/mongodb/mongo-client.js'
+// import * as redis from '../daos/impl/redis/redis-client.js'
+// import { App } from '../models/app.js'
+// import { Users } from '../models/users.js'
+import { _log, _error } from './logging.js'
+
+const log = _log.extend('utils:new-user')
+const error = _error.extend('utils:new-user')
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const appRoot = path.resolve(`${__dirname}/../..`)
+const appEnv = {}
+log(`appRoot: ${appRoot}`)
+dotenv.config({
+  path: path.resolve(appRoot, 'config/app.env'),
+  processEnv: appEnv,
+})
+// log(appEnv)
+// const mongoEnv = {}
+// dotenv.config({
+//   path: path.resolve(appRoot, 'config/mongodb.env'),
+//   processEnv: mongoEnv,
+//   debug: true,
+// })
+// log(mongoEnv)
+
+const program = new Command()
+program.name('refactor geo data')
+  .requiredOption('--first <name>', 'User\'s first name')
+  .requiredOption('--last <name>', 'User\'s last name')
+  .requiredOption('--email <addr>', 'User\'s email address')
+  .requiredOption(
+    '--desc <description>',
+    'Short description of the account',
+    'New account created using cli.',
+  )
+  .requiredOption('--password <password>', 'The new user\'s initial password.')
+  .option('-a, --admin', 'Make this user account admin, otherwise regular.')
+  .option('-t, --test', 'A test user account, not a real user.')
+
+program.parse(process.argv)
+const options = program.opts()
+// log(process.argv)
+log('options: ', options)
+
+const confEnv = {}
+if (options?.conf) {
+  dotenv.config({ path: path.resolve(appRoot, options.conf), processEnv: confEnv })
+}
+let email = confEnv?.email ?? options?.email ?? 'new_user@genevalakepiers.com'
+if (options?.test === true) {
+  const rando = crypto.randomBytes(2).toString('hex')
+  const at = options.email.indexOf('@')
+  email = `${options.email.slice(0, at)}-${rando}${options.email.slice(at)}`
+}
+
+const fName = confEnv?.FIRST_NAME ?? options?.first ?? 'Test'
+const lName = confEnv?.LAST_NAME ?? options?.last ?? 'Test'
+const password = confEnv?.PASSWORD ?? options?.password ?? null
+
+const ctx = {
+  app: {
+    root: appRoot,
+    dirs: {
+      public: {
+        dir: `${appRoot}/public`,
+      },
+      private: {
+        dir: `${appRoot}/private`,
+      },
+    },
+  },
+}
+
+const _id = new mongoClient.ObjectId()
+log(`new mongo _id: ${_id}`)
+
+const userProps = {
+  _id,
+  // first: options.first ?? 'First',
+  first: fName,
+  last: lName,
+  emails: [{ primary: email, verified: false }],
+  description: `A new (${(options?.test) ? 'test' : ''}) `
+    + `${(options?.admin) ? 'admin' : 'user'} account created from the cli.`,
+  username: `${fName.toLowerCase()}${lName.toLowerCase()}`,
+  password,
+  jwts: { token: '', refresh: '' },
+  userStatus: 'active',
+  schemaVer: 0,
+  ctx,
+  env: appEnv,
+  dbName: mongoClient.dbName,
+  client: mongoClient.client,
+}
+
+log(mongoClient.uri)
+log('[newUser] DB credentials in use: %O', userProps.client.options.credentials)
+log('[newUser] DB name in use: ', userProps.client.options.dbName)
+
+let newUser
+if (options.admin === true) {
+  newUser = Users.newAdminUser(userProps)
+} else {
+  newUser = Users.newUser(userProps)
+}
+try {
+  newUser.publicDir = 'a'
+  // TODO Fix issue with assigning privateDir looses hashed account directory in path.
+  // newUser.privateDir = 'a'
+  const newUserKeys = await newUser.generateKeys()
+  log(newUserKeys)
+} catch (e) {
+  error(e)
+  throw new Error(e.message, { cause: e })
+}
+let savedNewUser
+try {
+  savedNewUser = await newUser.save()
+  log(savedNewUser.privateDir)
+} catch (e) {
+  error(e)
+  throw new Error(e.message, { cause: e })
+}
+// Done creating new user account, exit process.
+process.exit()
